9dcb0bba99eec963f937dcaa4955313b858223e3
naveenreddy <naveenpeta29@gmail.com>
2025-06-16 19:29:01 +0530

    this the development of backend using mern

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors=require('cors');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(cors());
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/crud_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Start Server
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
